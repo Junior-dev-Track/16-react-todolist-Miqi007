47ab0ddbcedd9356451a0f2e071c36bc04a48f52
Miqi007 <louis.pavier@live.fr>
2024-04-22 11:41:28 +0200

    Checkbox ok + comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
 import React, { useState } from "react";
 
+
+//
 const TodoList = () => {
     const initialTodos = [
         { id: 1, title: "Learn React", isChecked: false },
         { id: 2, title: "Get some Holy Energy", isChecked: false }
     ];
 
+    // State
     const [todos, setTodos] = useState(initialTodos);
 
-    const toggleTodo = (id) => {
+    // Handle behaviour
+    const handleCheck = (id) => {
         setTodos(
             todos.map((todo) => {
-                if (todo.id === id) {
+                if (todo.id === id) { // catch the right checkbox to update
                     return {
                         ...todo, // updating todo in changing state
                         isChecked: !todo.isChecked,
@@ -26,7 +30,7 @@ const TodoList = () => {
 
 
 
-
+    // Render
     return (
         <>
             <section className="top">
@@ -41,7 +45,7 @@ const TodoList = () => {
                     {todos.map((todo) => (
                         // Assign a unique key to each list item 
                         <li key={todo.id}>
-                            <input type="checkbox" name="myCheckBox" checked={todo.isChecked} onChange={() => toggleTodo(todo.id)} /> {todo.title}
+                            <input type="checkbox" name="myCheckBox" checked={todo.isChecked} onChange={() => handleCheck(todo.id)} /> {todo.title}
                         </li>
                     ))}
                 </ul>
