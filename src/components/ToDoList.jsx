50479c6aaafce7c6b5a66693939be89743b326b6
Miqi007 <louis.pavier@live.fr>
2024-04-24 10:36:51 +0200

    Style checkbox

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,7 +3,10 @@ export default function ToDoList({ todoInfos, checkbox, deleteTodo }) {
     //render
     return (
         <li key={todoInfos.id} >
-            <input type="checkbox" name="myCheckBox" checked={todoInfos.isChecked} onChange={() => checkbox(todoInfos.id)} />
+            <label class="custom-checkbox">
+                <input type="checkbox" name="myCheckBox" checked={todoInfos.isChecked} onChange={() => checkbox(todoInfos.id)} />
+                <span class="checkmark"></span>
+            </label>
             <span style={{ textDecoration: todoInfos.isChecked ? 'line-through' : 'none' }}>{todoInfos.title}</span>
             <button onClick={() => deleteTodo(todoInfos.id)}>Delete</button>
         </li>
