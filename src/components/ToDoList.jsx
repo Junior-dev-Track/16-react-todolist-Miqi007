f5217bc59b36eb373fd657ca96e2d031074962c6
Miqi007 <louis.pavier@live.fr>
2024-04-24 12:23:27 +0200

    Edit function style

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,6 +4,8 @@ export default function ToDoList({ todoInfos, checkbox, deleteTodo, setUpdate })
 
     const [editing, setEditing] = useState(false);
     const [inputValue, setInputValue] = useState(todoInfos.title);
+    const [errorMessage, setErrorMessage] = useState('');
+
 
     // Edit item
     const handleEditing = () => {
@@ -13,8 +15,14 @@ export default function ToDoList({ todoInfos, checkbox, deleteTodo, setUpdate })
 
     const handleUpdatedDone = (event) => {
         if (event.key === 'Enter') {
-            setEditing(false);
-            setUpdate(inputValue, todoInfos.id); // Update the todo title using the setUpdate function
+            if (inputValue.trim() != "") { // test if it's not empty
+                setEditing(false);
+                setUpdate(inputValue, todoInfos.id); // Update the todo title using the setUpdate function
+                setErrorMessage("");
+            } else {
+                setErrorMessage('Empty task not allowed ⛔');
+
+            }
         }
     };
 
@@ -25,17 +33,22 @@ export default function ToDoList({ todoInfos, checkbox, deleteTodo, setUpdate })
 
     //render
     return (
-        <li key={todoInfos.id}>
-            <div className="view" style={viewMode}>
-                <label className="custom-checkbox">
-                    <input type="checkbox" name="myCheckBox" checked={todoInfos.isChecked} onChange={() => checkbox(todoInfos.id)} />
-                    <span className="checkmark"></span>
-                </label>
-                <span style={{ textDecoration: todoInfos.isChecked ? 'line-through' : 'none' }}>{todoInfos.title}</span>
-                <button onClick={() => deleteTodo(todoInfos.id)}>Delete</button>
-                <button onClick={handleEditing}>Edit</button>
-            </div>
-            <input type="text" value={inputValue} className="textInput" style={editMode} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleUpdatedDone} />
-        </li>
+        <>
+            <li key={todoInfos.id}>
+                <div className="view" style={viewMode}>
+                    <label className="custom-checkbox">
+                        <input type="checkbox" name="myCheckBox" checked={todoInfos.isChecked} onChange={() => checkbox(todoInfos.id)} />
+                        <span className="checkmark"></span>
+                    </label>
+                    <span style={{ textDecoration: todoInfos.isChecked ? 'line-through' : 'none' }}>{todoInfos.title}</span>
+                    <div className="btns">
+                        <button onClick={() => deleteTodo(todoInfos.id)}>Delete</button>
+                        <button onClick={handleEditing}>Edit</button>
+                    </div>
+                </div>
+                <input type="text" value={inputValue} className="textInput" style={editMode} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleUpdatedDone} />
+            </li>
+            {errorMessage && <div className="errorEdit"> {errorMessage} </div>}
+        </>
     )
 }
